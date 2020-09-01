b7b5c5e95d6c9dfd78cefc6a267da5ce799285c0
Moony4012 <42449752+Moony4012@users.noreply.github.com>
2020-09-01 19:38:06 +0200

    levier

--- Assets/Scenes/MoveBall.cs
+++ Assets/Scenes/MoveBall.cs
@@ -7,6 +7,8 @@ public class MoveBall : MonoBehaviour
     [SerializeField]
     Rigidbody2D rigidbody;
 
+    public Collider2D levier = null;
+
     private void Update()
     {
         if(Input.GetKey("right"))
@@ -17,6 +19,32 @@ public class MoveBall : MonoBehaviour
         {
             rigidbody.velocity -= new Vector2(1, 0);
         }
-        rigidbody.velocity += new Vector2(0, 1);
+        rigidbody.velocity += new Vector2(0, 0);
+
+        if (Input.GetKey(KeyCode.DownArrow)&& levier != null)
+        {
+            Debug.Log("nextlevel");
+        }
     }
+
+    private void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        if (collider2D.tag == "levier")
+        {
+            levier = collider2D;
+
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collider2D)
+    {
+        if (collider2D.tag == "levier")
+        {
+            levier = null;
+
+        }
+
+    }
+
 }
