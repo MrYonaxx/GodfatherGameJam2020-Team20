6305102be0cce57dad5f776d7308eccc787f6844
Nam <nam.mirbeau@gmail.com>
2020-09-01 18:04:40 +0200

    GitIgnore update

---
+++ Assets/Scenes/MoveBall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBall : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody2D rigidbody;
+
+    private void Update()
+    {
+        if(Input.GetKey("right"))
+        {
+            rigidbody.velocity += new Vector2(1, 0);
+        }
+        else if (Input.GetKey("left"))
+        {
+            rigidbody.velocity -= new Vector2(1, 0);
+        }
+        rigidbody.velocity += new Vector2(0, 1);
+    }
+}
