3cabb347f4909881621bf479144bc83e644d2dd2
m3t4tr4p <loing.francois@gmail.com>
2020-09-03 19:13:55 +0200

    Ajout de la fresque du haut

---
+++ Assets/Sprite/Fresque/CleanBoy/Cleanboy_Moving.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cleanboy_Moving : MonoBehaviour
+{
+    public float vitesse = 10;
+    public float xmax = 100;
+    public float xbase = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position += new Vector3(vitesse,0)*Time.deltaTime;
+            if(gameObject.transform.position.x >= xmax)
+            {
+                gameObject.transform.position = new Vector3(xbase,gameObject.transform.position.y,gameObject.transform.position.z);
+            }
+    }
+}
