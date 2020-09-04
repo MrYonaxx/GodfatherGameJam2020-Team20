0908335dd632c4f989e3ee2e016a3b1dc4ba4385
Leo Nimsgern <nimsgernleo@gmail.com>
2020-09-04 10:11:37 +0200

    menu

--- Assets/Script/Menu.cs
+++ Assets/Script/Menu.cs
@@ -10,9 +10,14 @@ public class Menu : MonoBehaviour
 {
     public GameObject Control;
     public GameObject Main_Menu;
+    public GameObject button;
     public Selectable ControlDefautFocus;
     public Selectable Main_MenuDefautFocus;
-
+    public EventSystem eventSystem;
+    public void Buton()
+    {
+        StartCoroutine(FocusEventSystem(button));
+    }
     public void OnStart()
     {
         SceneManager.LoadScene("Lvl 1");
@@ -33,5 +38,11 @@ public class Menu : MonoBehaviour
     {
         Control.SetActive(false);
         Main_Menu.SetActive(true);
+    }
+    public IEnumerator FocusEventSystem(GameObject button)
+    {
+        yield return null;
+        eventSystem.SetSelectedGameObject(null);
+        eventSystem.SetSelectedGameObject(button);
     }
 }
